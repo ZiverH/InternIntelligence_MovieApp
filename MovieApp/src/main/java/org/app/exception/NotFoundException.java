bdeed08398638ab4af27ac77918a34c8ae6b90e0
ZiverH <ziver.hesenova.2020@gmail.com>
2025-02-09 21:09:47 +0400

    movie api

--- MovieApp/src/main/java/org/app/exception/NotFoundException.java
+++
@@ -1,9 +0,0 @@
-package org.app.exception;
-
-
-import lombok.AllArgsConstructor;
-
-@AllArgsConstructor
-public class NotFoundException extends RuntimeException {
-    private String entityName;
-}
--- MovieApp/src/main/java/org/app/repository/MovieRepository.java
+++
@@ -1,7 +0,0 @@
-package org.app.repository;
-
-import org.app.model.Movie;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MovieRepository extends JpaRepository<Movie, Long> {
-}
